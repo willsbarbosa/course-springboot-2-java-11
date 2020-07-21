21ff85b1a39033dd5b6f8ea1d3b6d8a3e4a4c180
William Barbosa <barbosa_sp2000@hotmail.com>
2020-07-21 17:41:20 -0300

    Order, Instant, ISO 8601 part 1

--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -1,13 +1,18 @@
 package com.example.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -20,6 +25,9 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 		
 	}
@@ -72,6 +80,11 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -96,7 +109,4 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
 }
--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -30,5 +30,5 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
-	}
+	} 
 }
